b0865116cac92175495ad49a346a7296a5af4e50
alexsaushkin <thirdyou@yandex.ru>
2023-04-28 17:57:30 +0400

    add: add models, basic routes and basic controllers

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+// миддлвэр, добавляющий пользователя
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133',
+  };
+
+  next();
+});
+
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
---
+++ controllers/cards.js
@@ -0,0 +1,23 @@
+const Card = require('../models/cards');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.deleteCard = (req, res) => {
+  const { cardId } = req.params;
+
+  Card.findByIdAndRemove(cardId)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
