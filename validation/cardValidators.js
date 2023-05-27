6a00e6268f66dd9e86164b4cc3adc8de528ab074
alexsaushkin <thirdyou@yandex.ru>
2023-05-27 18:05:35 +0400

    fix: fix cardId validator

--- validation/cardValidators.js
+++ validation/cardValidators.js
@@ -10,7 +10,7 @@ const validateCard = celebrate({
 
 const validateCardId = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.objectId().required(),
+    cardId: Joi.string().length(24).hex().required(),
   }),
 });
 
